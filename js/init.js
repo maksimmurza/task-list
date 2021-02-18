f16545569b9e4f856e06246c93719df408809b95
Maksim Murza <maksim.murza.v@gmail.com>
2021-02-19 00:28:25 +0300

    fully responsive adding form

--- js/init.js
+++ js/init.js
@@ -2,5 +2,5 @@ export {tabs, tabsAreas, addButton, input};
 
 let tabs = document.querySelectorAll('.tab');
 let tabsAreas = document.querySelectorAll('.list');
-let addButton = document.querySelector('.add');
-let input = document.querySelector('.input');
+let addButton = document.querySelector('.add-task-button');
+let input = document.querySelector('.task-name-input');
--- js/main.js
+++ js/main.js
@@ -9,50 +9,60 @@ function main() {
     tabs.forEach(tab => {tab.addEventListener('click', () => {openTab(tab)})});
 }
 
+function openTab(tab) {
+
+    // Making previous tab and it's area inactive
+    document.querySelector('.active-tab').classList.remove('active-tab');
+    document.querySelector('.active-list').classList.remove('active-list');
+
+    // Making pressed tab and it's area active
+    tab.classList.add('active-tab');
+    let activeArea = document.querySelector('.list-'+tab.textContent.toLowerCase());
+    activeArea.classList.add('active-list');
+}
+
 function addNewTask(event) {
 
+    // Prevent reloading with novalidate or html validation message after adding new task
     event.preventDefault();
 
-    let newTask = document.createElement('div');
-    let taskName = document.createElement('span');
-    let deleteButton = document.createElement('button');
-    let checkboxButton = document.createElement('button');
-    let activeList = document.querySelector('.active-list');
-
-    newTask.classList.add('task');
-    deleteButton.classList.add('delete', 'fas', 'fa-trash');
-    checkboxButton.classList.add('check', 'far', 'fa-check-circle');
-
+    // Checking on empty field and string contains spaces only
     if(!input.validity.valid && !input.value.replace(/\s/g, '').length) {
         alert("Name can not be empty and must have a characters");
         input.value = '';
         return;
     }
 
+    // Building html-block for task
+    let newTask = document.createElement('div');
+    let taskName = document.createElement('span');
+    let deleteButton = document.createElement('button');
+    let completeButton = document.createElement('button');
+    let activeList = document.querySelector('.active-list');
+
+    newTask.classList.add('task');
+    taskName.classList.add('task-name');
+    deleteButton.classList.add('delete', 'fas', 'fa-trash');
+    completeButton.classList.add('complete', 'far', 'fa-check-circle');
+
     taskName.textContent = input.value;
     input.value = '';
-    newTask.appendChild(checkboxButton);
+
+    newTask.appendChild(completeButton);
     newTask.appendChild(taskName);
     newTask.appendChild(deleteButton);
     activeList.appendChild(newTask);
 
+    // Listeners on buttons of newly created task block 
     deleteButton.onclick = (event) => {deleteTask(event)};
-    checkboxButton.onclick = (event) => {checkTask(event)};
-}
-    
-function openTab(tab) {
-    document.querySelector('.active-tab').classList.remove('active-tab');
-    document.querySelector('.active-list').classList.remove('active-list');
-    tab.classList.add('active-tab');
-    let activeArea = document.querySelector('.list-'+tab.textContent.toLowerCase());
-    activeArea.classList.add('active-list');
+    completeButton.onclick = (event) => {completeTask(event)};
 }
 
 function deleteTask(event) {
     event.target.parentNode.remove();
 }
 
-function checkTask(event) {
+function completeTask(event) {
     if(event.target.style.color != 'green') {
         event.target.style.color = 'green';
         event.target.parentNode.querySelector('span').style.textDecoration = 'line-through';
